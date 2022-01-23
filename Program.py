dbd24a1ea951b9a5ce4774afd85ca80be3742b2e
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-01-23 11:53:52 -0800

    Request to upload data 1.23.22

--- Program.py
+++ Program.py
@@ -167,7 +167,7 @@ from matplotlib.collections import PathCollection
 from matplotlib.collections import PatchCollection
 from matplotlib.collections import
 
-=======
+=#======
 
 import os
 import time
@@ -221,7 +221,7 @@ from mat
 import sys
 import
 
-=======
+#=======
 
 import os
 import time
@@ -275,7 +275,7 @@ from mat
 import os
 import
 
-=======
+#=======
 
 import os
 import time
@@ -327,7 +327,7 @@ from matplotlib.collections import PathCollection
 from matplotlib.collections import PatchCollection
 from matplotlib
 
-=======
+#=======
 
 import os
 import subprocess
@@ -516,6 +516,7 @@ import os
 os.system("python3.6 -m emuai.emuai -w")
 ## write the os that writes the data to
 
+
 =======
 
 import os
@@ -546,7 +547,6 @@ import matplotlib.colors as colors
 import matplotlib.colorbar as cbar
 from matplotlib.collections import PatchCollection
 from matplotlib.ticker import MaxNLocator
-from matplotlib import rc
 from matplotlib import ticker
 from matplotlib.ticker import FormatStrFormatter
 from matplotlib.ticker import LinearLocator
@@ -575,3 +575,246 @@ from matplotlib.lines import Line2D
 from matplotlib.patches import Circle
 from matplotlib.collections import PatchCollection
 from matplotlib.collections import Line
+## write a gui in t
+
+=======
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.colors import LogNorm
+from matplotlib.colors import Normalize
+from matplotlib.colors import PowerNorm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+from matplotlib.patches import RegularPolygon
+from matplotlib.patches import Polygon
+from matplotlib.patches import Wedge
+from matplotlib.patches import Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.lines import Line2D
+from matplotlib.markers import MarkerStyle
+from matplotlib.markers import Marker
+from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import Line
+## write a gui in tkinter for showing the game
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from
+
+#=======
+
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.font as tkfont
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkmb
+import tkinter.simpledialog as tksd
+import tkinter.scrolledtext as tksct
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkf
+import tkinter.ttk as tkt
+import tkinter.tix as tix
+import tkinter.constants as tkc
+import tkinter.constants as tkconst
+import tkinter.commondialog as tkcd
+import tkinter.dialog as tkd
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkmb
+import tkinter.simpledialog as tksd
+import tkinter.scrolledtext as tksct
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkf
+import tkinter.ttk as tkt
+import tkinter.tix as tix
+import tkinter.constants as tkc
+import tkinter.constants as tkconst
+import tkinter.commondialog as tkcd
+import tkinter.dialog as tkd
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkmb
+import tkinter.simpledialog as tksd
+import tkinter.scrolledtext as tksct
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkf
+import tkinter.ttk as tkt
+import tkinter.tix as tix
+import tkinter.constants as tkc
+import tkinter.constants as tkconst
+import tkinter.commondialog as tkcd
+import tkinter.dialog as tkd
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as
+
+#=======
+
+import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.font as tkFont
+import tkinter.filedialog as tkFileDialog
+import tkinter.messagebox as tkMessageBox
+import tkinter.scrolledtext as tkScrolledText
+import tkinter.simpledialog as tkSimpleDialog
+import tkinter.colorchooser as tkColorChooser
+import tkinter.tix as tix
+import tkinter.constants as tkConstants
+import tkinter.font as tkFont
+import tkinter.fontchooser as tkFontChooser
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+import tkinter.simpledialog as tkSimpleDialog
+import tkinter.colorchooser as tkColorChooser
+import tkinter.tix as tix
+import tkinter.constants as tkConstants
+import tkinter.font as tkFont
+import tkinter.fontchooser as tkFontChooser
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+import tkinter.simpledialog as tkSimpleDialog
+import tkinter.colorchooser as tkColorChooser
+import tkinter.tix as tix
+import tkinter.constants as tkConstants
+import tkinter.font as tkFont
+import tkinter.fontchooser as tkFontChooser
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+import tkinter.simpledialog as tkSimpleDialog
+import tkinter.colorchooser as tkColorChooser
+import tkinter.tix as tix
+import tkinter.constants as tkConstants
+import tkinter.font as tkFont
+import tkinter.fontchooser as tkFontChooser
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMessageBox
+import tkinter.simpledialog as
+
+#=======
+
+import tkinter as tk
+import tkinter.font as tkfont
+import tkinter.ttk as ttk
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkm
+import tkinter.scrolledtext as tkst
+import tkinter.simpledialog as tksd
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkfont
+import tkinter.ttk as ttk
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkm
+import tkinter.scrolledtext as tkst
+import tkinter.simpledialog as tksd
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkfont
+import tkinter.ttk as ttk
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkm
+import tkinter.scrolledtext as tkst
+import tkinter.simpledialog as tksd
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkfont
+import tkinter.ttk as ttk
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkm
+import tkinter.scrolledtext as tkst
+import tkinter.simpledialog as tksd
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkfont
+import tkinter.ttk as ttk
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkm
+import tkinter.scrolledtext as tkst
+import tkinter.simpledialog as tksd
+import tkinter.colorchooser as tkcc
+import tkinter.font as tkfont
+import tkinter.ttk as ttk
+import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkm
+import tkinter.scrolledtext as
+
+#=======
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import colorchooser
+from tkinter import scrolledtext
+from tkinter import Menu
+from tkinter import Spinbox
+from tkinter import OptionMenu
+from tkinter import Checkbutton
+from tkinter import Radiobutton
+from tkinter import IntVar
+from tkinter import StringVar
+from tkinter import DoubleVar
+from tkinter import BooleanVar
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import colorchooser
+from tkinter import scrolledtext
+from tkinter import Menu
+from tkinter import Spinbox
+from tkinter import OptionMenu
+from tkinter import Checkbutton
+from tkinter import Radiobutton
+from tkinter import IntVar
+from tkinter import StringVar
+from tkinter import DoubleVar
+from tkinter import BooleanVar
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import colorchooser
+from tkinter import scrolledtext
+from tkinter import Menu
+from tkinter import Spinbox
+from tkinter import OptionMenu
+from tkinter import Checkbutton
+from tkinter import Radiobutton
+from tkinter import IntVar
+from tkinter import StringVar
+from tkinter import DoubleVar
+from tkinter import BooleanVar
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import colorchooser
+from tkinter import scrolledtext
+from tkinter
+## write the mips cpu data loader

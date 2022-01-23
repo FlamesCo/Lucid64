c19a8527f736f05871bae44cd3e499dd1e723462
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-01-23 11:39:21 -0800

    Request to upload 1.23.22

---
+++ Program.py
@@ -0,0 +1,577 @@
+### EMUAI (c) N64 LAB TEAM
+
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QMessageBox
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import Qt
+import sys
+import os
+import re
+import time
+import random
+import subprocess
+import threading
+import json
+from datetime import datetime
+from pynput.keyboard import Key, Listener
+import platform
+import socket
+import base64
+import hashlib
+import requests
+from PyQt5.QtCore import QThread
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QEvent
+from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QPainter, QPen, QBrush, QColor, QFont
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QLabel, QLineEdit, QMessageBox
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QLineEdit, QMessageBox, QFileDialog
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QLineEdit, QMessageBox, QFileDialog
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.Q
+
+ 
+
+import sys
+import os
+import time
+import random
+import math
+import numpy as np
+import numpy.random as npr
+import scipy.stats as sps
+import scipy.misc as spm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rc
+from matplotlib import ticker
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.colors import LogNorm
+from matplotlib.colors import Normalize
+from matplotlib.colors import PowerNorm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+from matplotlib.patches import RegularPolygon
+from matplotlib.patches import Polygon
+from matplotlib.patches import Wedge
+from matplotlib.patches import Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.lines import Line2D
+from matplotlib.markers import MarkerStyle
+from matplotlib.markers import Marker
+from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import LineCollection
+from matplotlib.collections import PolyCollection
+from matplotlib.collections import PathCollection
+from matplotlib.collections import PatchCollection
+from mat
+
+import sys
+import os
+import time
+import random
+import numpy as np
+import numpy.random as npr
+import scipy.stats as sps
+## make a project 64 like gui
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+## write a n64 like gui
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import messagebox
+
+print("Welcome to EMUN64 Please insert rom")
+## write a mips 64 like rom loader
+import os
+rom = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("n64 rom files","*.z64"),("all files","*.*")))
+os.system("mips64 -n -m -f " + rom)
+## write the os that reads the data from n64
+import os
+import time
+import random
+import numpy as np
+import numpy.random as npr
+import scipy.stats as sps
+import scipy.misc as spm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rc
+from matplotlib import ticker
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.colors import LogNorm
+from matplotlib.colors import Normalize
+from matplotlib.colors import PowerNorm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+from matplotlib.patches import RegularPolygon
+from matplotlib.patches import Polygon
+from matplotlib.patches import Wedge
+from matplotlib.patches import Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.lines import Line2D
+from matplotlib.markers import MarkerStyle
+from matplotlib.markers import Marker
+from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import LineCollection
+from matplotlib.collections import PolyCollection
+from matplotlib.collections import PathCollection
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import
+
+=======
+
+import os
+import time
+import random
+import numpy as np
+import numpy.random as npr
+import scipy.stats as sps
+import scipy.misc as spm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rc
+from matplotlib import ticker
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.colors import LogNorm
+from matplotlib.colors import Normalize
+from matplotlib.colors import PowerNorm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+from matplotlib.patches import RegularPolygon
+from matplotlib.patches import Polygon
+from matplotlib.patches import Wedge
+from matplotlib.patches import Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.lines import Line2D
+from matplotlib.markers import MarkerStyle
+from matplotlib.markers import Marker
+from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import LineCollection
+from matplotlib.collections import PolyCollection
+from matplotlib.collections import PathCollection
+from matplotlib.collections import PatchCollection
+from mat
+
+import sys
+import
+
+=======
+
+import os
+import time
+import random
+import numpy as np
+import numpy.random as npr
+import scipy.stats as sps
+import scipy.misc as spm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rc
+from matplotlib import ticker
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.colors import LogNorm
+from matplotlib.colors import Normalize
+from matplotlib.colors import PowerNorm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+from matplotlib.patches import RegularPolygon
+from matplotlib.patches import Polygon
+from matplotlib.patches import Wedge
+from matplotlib.patches import Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.lines import Line2D
+from matplotlib.markers import MarkerStyle
+from matplotlib.markers import Marker
+from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import LineCollection
+from matplotlib.collections import PolyCollection
+from matplotlib.collections import PathCollection
+from matplotlib.collections import PatchCollection
+from mat
+
+import os
+import
+
+=======
+
+import os
+import time
+import subprocess
+import random
+import numpy as np
+import numpy.random as npr
+import scipy.stats as sps
+import scipy.misc as spm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rc
+from matplotlib import ticker
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.colors import LogNorm
+from matplotlib.colors import Normalize
+from matplotlib.colors import PowerNorm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+from matplotlib.patches import RegularPolygon
+from matplotlib.patches import Polygon
+from matplotlib.patches import Wedge
+from matplotlib.patches import Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.lines import Line2D
+from matplotlib.markers import MarkerStyle
+from matplotlib.markers import Marker
+from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import LineCollection
+from matplotlib.collections import PolyCollection
+from matplotlib.collections import PathCollection
+from matplotlib.collections import PatchCollection
+from matplotlib
+
+=======
+
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import sys
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import base64
+import hashlib
+import requests
+from datetime import datetime
+import platform
+import os
+import subprocess
+import re
+import time
+import random
+import socket
+import json
+import
+
+=======
+
+import os
+os.system("python3.6 -m emuai.emuai")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to n64
+import os
+os.system("python3.6 -m emuai.emuai -w")
+## write the os that writes the data to
+
+=======
+
+import os
+import time
+import random
+import json
+import socket
+import requests
+import base64
+import hashlib
+import threading
+import subprocess
+import platform
+import sys
+import os
+import re
+import time
+import random
+import numpy as np
+import numpy.random as npr
+import scipy.stats as sps
+import scipy.misc as spm
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import matplotlib.colorbar as cbar
+from matplotlib.collections import PatchCollection
+from matplotlib.ticker import MaxNLocator
+from matplotlib import rc
+from matplotlib import ticker
+from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.colors import LogNorm
+from matplotlib.colors import Normalize
+from matplotlib.colors import PowerNorm
+from matplotlib.colors import SymLogNorm
+from matplotlib.colors import BoundaryNorm
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+from matplotlib.patches import Ellipse
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+from matplotlib.patches import RegularPolygon
+from matplotlib.patches import Polygon
+from matplotlib.patches import Wedge
+from matplotlib.patches import Arc
+from matplotlib.collections import PatchCollection
+from matplotlib.lines import Line2D
+from matplotlib.markers import MarkerStyle
+from matplotlib.markers import Marker
+from matplotlib.lines import Line2D
+from matplotlib.patches import Circle
+from matplotlib.collections import PatchCollection
+from matplotlib.collections import Line
